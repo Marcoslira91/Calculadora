94216748e3da31c5fd315777479c9c8cb2b2920c
Marcos Lira <53446701+Marcoslira91@users.noreply.github.com>
2022-08-15 18:14:23 -0300

    Add files via upload

---
+++ src/Calcular.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Calcular {
+
+	public static void main(String[] args) {		
+		Scanner leia = new Scanner(System.in);		
+		Formulas calc = new Formulas();
+		
+		int opcao;
+		double valor1, valor2;
+		
+		
+		System.out.println("Programa Calculadora");
+		System.out.println("Informe uma das opções a seguir");
+		System.out.println("1 - Somar | 2 - Subtrair | 3 - Multiplicar | 4 - Dividir | 0 - Sair");
+		opcao = leia.nextInt();
+		
+		if(opcao == 0) {
+			System.out.println("Você saiu do programa.");
+		}else {
+			System.out.print("Informe o primeiro valor: ");
+			valor1 = leia.nextDouble();
+			System.out.print("Informe o segundo valor: ");
+			valor2 = leia.nextDouble();
+			
+			switch (opcao) {
+			case 1:
+				System.out.println("A soma de "+valor1+" + "+valor2+" é: "+calc.soma(valor1, valor2));
+				break;
+			
+			case 2:
+				System.out.println("A subtração de "+valor1+" + "+valor2+" é: "+calc.subtrair(valor1, valor2));
+				break;
+				
+			case 3:
+				System.out.println("A multiplicação de "+valor1+" + "+valor2+" é: "+calc.multiplicar(valor1, valor2));
+				break;
+			
+			case 4:
+				System.out.println("A divisão de "+valor1+" + "+valor2+" é: "+calc.dividir(valor1, valor2));
+				break;
+				
+			default:
+				System.out.println("Informe um valor válido!");
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ src/Formulas.java
@@ -0,0 +1,19 @@
+
+public class Formulas {
+	
+	public double soma(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+	public double subtrair(double num1, double num2) {
+		return num1 - num2;
+	}
+	
+	public double multiplicar(double num1, double num2) {
+		return num1 * num2;
+	}
+	
+	public double dividir(double num1, double num2) {
+		return num1 / num2;
+	}
+}
